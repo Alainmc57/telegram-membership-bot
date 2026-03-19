64ca2e0545002fc568292c9e5c6fb1c188a36607
Alainmc57 <127628172+Alainmc57@users.noreply.github.com>
2026-03-18 20:10:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
+
+TOKEN = os.environ.get("BOT_TOKEN")
+TELEGRAM_API = f"https://api.telegram.org/bot{TOKEN}"
 
 @app.route("/")
 def home():
     return "Bot activo"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        reply = f"Recibí tu mensaje: {text}"
+
+        requests.post(
+            f"{TELEGRAM_API}/sendMessage",
+            json={
+                "chat_id": chat_id,
+                "text": reply
+            }
+        )
+
+    return "ok"
